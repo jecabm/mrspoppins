fa8b4d2999220e580e74a585dc406256008dc3f3
jecabm <32487257+jecabm@users.noreply.github.com>
2021-04-11 23:18:10 +1000

    update

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("burger").style.display = "none";
+  document.getElementById("sociale").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("sociale").style.height = "0%";
+  document.getElementById("burger").style.display = "block";
+}
